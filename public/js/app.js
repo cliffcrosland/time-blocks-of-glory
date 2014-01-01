9171812832349f3b8102ec02a16250e5776fc940
Cliff Crosland <cliffcrosland@gmail.com>
2014-01-01 10:56:29 -0800

    Begin handling keydown events.

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,52 @@
 angular.module('App', []);
 
 angular.module('App')
-.controller('AppCtrl', ['$scope', function ($scope) {
+.controller('AppCtrl', ['$scope', '$document', function ($scope, $document) {
 
   var startHour = 5;
   var endHour = 23;
   $scope.halfHours = [];
-  for (var halfHour = startHour; halfHour <= endHour; halfHour += 0.5) {
-    $scope.halfHours.push(halfHour);
+  for (var time = startHour; time <= endHour; time += 0.5) {
+    $scope.halfHours.push({ time: time });
   }
 
-  $scope.showHalfHour = function (halfHour) {
-    return halfHour == Math.floor(halfHour);
+  $scope.showTime = function (time) {
+    return time == Math.floor(time);
   };
 
-  $scope.formatHalfHour = function (halfHour) {
-    var time = halfHour;
+  $scope.formatTime = function (time) {
     if (time > 12) {
       time -= 12;
     }
     return time;
+  }
+
+  $document.bind('keydown', function (evt) {
+    preventDefaults(evt);
+    if (isCtrlA(evt)) {
+      addBlock();
+    }
+    console.log(evt);
+  });
+
+  function isCtrlA(evt) {
+    var ctrlKeyPressed = (evt.ctrlKey || evt.metaKey);
+    var letterAPressed = (evt.keyCode == 65);
+    return letterAPressed && ctrlKeyPressed;
+  }
+
+  function isUpOrDownArrow(evt) {
+    return evt.keyCode == 40 || evt.keyCode == 38;
+  }
+
+  function preventDefaults(evt) {
+    if (isCtrlA(evt) || isUpOrDownArrow(evt)) {
+      evt.preventDefault();
+    }
+  }
+
+  function addBlock() {
+    console.log('should add time block');
   }
 
 }]);
